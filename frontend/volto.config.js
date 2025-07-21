a0a6c72d04749fb9cf3ea6f5fef0060b1bae5928
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2025-07-21 22:01:30 +0200

    Updated the '@kitconcept/volto-separator-block' configurations #10

--- frontend/volto.config.js
+++ frontend/volto.config.js
@@ -3,6 +3,7 @@ const addons = [
   '@kitconcept/volto-heading-block',
   '@kitconcept/volto-highlight-block',
   '@kitconcept/volto-introduction-block',
+  '@kitconcept/volto-separator-block',
   'volto-sevilla-imd-website',
 ];
 const theme = '@kitconcept/volto-light-theme';
