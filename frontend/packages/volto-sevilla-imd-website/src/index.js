ad655e852367131db56b2ab3325c48600bcf381f
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2025-07-23 01:45:14 +0200

    Updated the 'volto-gdpr-privacy' configurations for the frontend app #21

--- frontend/packages/volto-sevilla-imd-website/src/index.js
+++ frontend/packages/volto-sevilla-imd-website/src/index.js
@@ -1,7 +1,15 @@
 import installSettings from 'volto-sevilla-imd-website/config/settings';
+import gdprPrivacySettings from 'volto-sevilla-imd-website/config/addons/volto-gdpr-privacy';
 
 const applyConfig = (config) => {
   installSettings(config);
+
+  // GDPR Privacy
+  config.settings['volto-gdpr-privacy'] = {
+    ...config.settings['volto-gdpr-privacy'],
+    defaultPanelConfig: gdprPrivacySettings,
+  };
+
   return config;
 };
 
