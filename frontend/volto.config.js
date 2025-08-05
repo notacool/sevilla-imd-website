baa43182e1f556d49fa055cee3b49bc93af5a28b
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2025-08-06 00:08:44 +0200

    Temporarily removed the volto-dropdownmenu' addon for the frontend website #19

--- frontend/volto.config.js
+++ frontend/volto.config.js
@@ -1,7 +1,6 @@
 const addons = [
   '@eeacms/volto-accordion-block',
   'volto-form-block',
-  'volto-dropdownmenu',
   'volto-gdpr-privacy',
   '@mbarde/volto-fullcalendar-block',
   '@kitconcept/volto-banner-block',
