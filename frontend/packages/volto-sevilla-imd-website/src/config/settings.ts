4e78ba6c948eb7602d8a9df672c4852398d77c29
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2025-08-06 00:26:47 +0200

    Enable configuration of the 'Fat Menu' feature for the frontend website #35

--- frontend/packages/volto-sevilla-imd-website/src/config/settings.ts
+++ frontend/packages/volto-sevilla-imd-website/src/config/settings.ts
@@ -3,6 +3,7 @@ import type { ConfigType } from '@plone/registry';
 export default function install(config: ConfigType) {
   // Main
   config.settings.siteLabel = 'Instituto Municipal de Deportes - Ayuntamiento de Sevilla';
+
   // Language
   config.settings.isMultilingual = false;
   config.settings.supportedLanguages = ['es'];
@@ -10,8 +11,21 @@ export default function install(config: ConfigType) {
 
   // Display Plone Login Form
   config.settings.showPloneLogin = true;
+
   // Navigation
   config.settings.navDepth = 3;
 
+  // Expanders
+  config.settings.apiExpanders = [
+    ...config.settings.apiExpanders,
+    {
+      match: '',
+      GET_CONTENT: ['inherit'],
+      querystring: {
+        'expand.inherit.behaviors':
+          'voltolighttheme.header',
+      },
+    },
+  ];
   return config;
 }
