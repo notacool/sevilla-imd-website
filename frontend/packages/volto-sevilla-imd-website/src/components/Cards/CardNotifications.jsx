298f9f70e32ab0bf55a302157c10c75a4cf6c549
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2025-08-26 05:43:13 +0200

    Added the initial CardNotifications component from Home layout and its SCSS styling #33

---
+++ frontend/packages/volto-sevilla-imd-website/src/components/Cards/CardNotifications.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { UniversalLink } from '@plone/volto/components';
+import { Image } from 'semantic-ui-react';
+import DefaultImageSVG from '@plone/volto/components/manage/Blocks/Listing/default-image.svg';
+import ArrowRightDiagonalYellowIcon from './arrow-right-diagonal-yellow.svg';
+// import 'volto-sevilla-imd-website/theme/components/_card_notifications.scss';
+
+const CardNotifications = ({
+  status,
+  title,
+  image,
+  link,
+  linkText = 'Hiperlink',
+}) => {
+  return (
+    <div className="card-notifications">
+      <div className="card-content">
+        <div className="notification-content">
+          {/* Notification Image */}
+          <div className="notification-image">
+            <div
+              className="image-wrapper"
+              style={{
+                backgroundImage: `url(${image || DefaultImageSVG})`,
+              }}
+            />
+          </div>
+
+          {/* Notification Details */}
+          <div className="notification-details">
+            <div className="text-content">
+              <div className="notification-status">
+                {status || 'Inscripción abierta'}
+              </div>
+
+              <h3 className="notification-title">
+                {title || 'Zurich Maratón de Sevilla 2026'}
+              </h3>
+
+              {link && (
+                <UniversalLink
+                  href={link}
+                  className="notification-link"
+                >
+                  <span>{linkText}</span>
+                  <div className="icon">
+                    <Image src={ArrowRightDiagonalYellowIcon} title="Más información" />
+                  </div>
+                </UniversalLink>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CardNotifications;
