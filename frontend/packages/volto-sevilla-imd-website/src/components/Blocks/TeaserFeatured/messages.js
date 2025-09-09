1f44cc0072303adfc5e331f039b3494bd3602a19
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2025-09-09 18:11:39 +0200

    Added the 'TeaserFeatured' block as a replace to `S1` component #59

---
+++ frontend/packages/volto-sevilla-imd-website/src/components/Blocks/TeaserFeatured/messages.js
@@ -0,0 +1,33 @@
+/**
+ * TeaserFeatured Block Messages.
+ */
+import { defineMessages } from 'react-intl';
+
+const messages = defineMessages({
+  teaserFeaturedBlock: {
+    id: 'TeaserFeaturedBlock',
+    defaultMessage: 'Teaser Featured Block',
+  },
+  subtitle: {
+    id: 'Subtitle',
+    defaultMessage: 'Subtitle',
+  },
+  title: {
+    id: 'Title',
+    defaultMessage: 'Title',
+  },
+  description: {
+    id: 'Description',
+    defaultMessage: 'Description',
+  },
+  button: {
+    id: 'Button text',
+    defaultMessage: 'Button text',
+  },
+  target: {
+    id: 'Button Target',
+    defaultMessage: 'Button Target',
+  },
+});
+
+export default messages;
