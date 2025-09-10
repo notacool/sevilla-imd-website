61340142e0ea2b74bbf75d0b8d7ea9d0ba8a50c5
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2025-09-10 16:26:42 +0200

    Updated the 'S3' component #61

--- frontend/packages/volto-sevilla-imd-website/src/components/Cards/CardFeatured.jsx
+++ frontend/packages/volto-sevilla-imd-website/src/components/Cards/CardFeatured.jsx
@@ -1,10 +1,13 @@
+/**
+ * Card Featured component.
+ * @module components/Cards/CardFeatured
+ */
+
 import React from 'react';
 import { UniversalLink } from '@plone/volto/components';
 import { Image } from 'semantic-ui-react';
-import Icon from '@plone/volto/components/theme/Icon/Icon';
 import DefaultImageSVG from '@plone/volto/components/manage/Blocks/Listing/default-image.svg';
-import LinkExternalIcon from './link-external.png';
-// import 'volto-sevilla-imd-website/theme/components/_card_featured.scss';
+import LinkExternalIcon from 'volto-sevilla-imd-website/icons/link-external.png';
 
 const CardFeatured = ({
   title,
