90c414bf700e4dbe9f3bdaad8444c773b51aa735
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2025-09-11 14:36:03 +0200

    Added the 'CardCallAction' component #77

---
+++ frontend/packages/volto-sevilla-imd-website/src/components/Cards/CardCallAction.jsx
@@ -0,0 +1,50 @@
+/**
+ * Card Call Action component.
+ * @module components/Cards/CardCallAction
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import ArrowRightYellowIcon from 'volto-sevilla-imd-website/icons/arrow-right-yellow.png';
+
+const CardCallAction = ({
+  title,
+  description,
+  buttonText,
+  className,
+  onClick,
+}) => {
+  return (
+    <div className={classNames('card-call-action', className)}>
+      <div className="card-content">
+        <h3 className="card-title">
+          {title || 'Escuelas Deportivas Municipales de Iniciación'}
+        </h3>
+
+        <p className="card-description">
+          {description || 'Prebenjamín a Infantil, y escuelas para personas con discapacidad.'}
+        </p>
+
+        <button className="action-button" onClick={onClick}>
+          <span className="button-text">
+            {buttonText || 'INICIACIÓN'}
+          </span>
+          <span className="button-icon">
+            <img src={ArrowRightYellowIcon} alt={buttonText || 'INICIACIÓN'} />
+          </span>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+CardCallAction.propTypes = {
+  title: PropTypes.string,
+  description: PropTypes.string,
+  buttonText: PropTypes.string,
+  className: PropTypes.string,
+  onClick: PropTypes.func,
+};
+
+export default CardCallAction;
