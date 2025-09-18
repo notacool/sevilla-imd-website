5a4ac9554d481cb0d96f3f8794f11c161508f29a
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2025-09-19 01:14:24 +0200

    Updated the 'S3' component #61

---
+++ frontend/packages/volto-sevilla-imd-website/src/components/Cards/index.js
@@ -0,0 +1,11 @@
+/**
+ * Index file for Cards components.
+ * @module components/Cards
+ */
+
+import CardFeatured from './CardFeatured';
+import CardEvents from './CardEvents';
+import CardNews from './CardNews';
+import CardNotifications from './CardNotifications';
+
+export { CardFeatured, CardEvents, CardNews, CardNotifications };
