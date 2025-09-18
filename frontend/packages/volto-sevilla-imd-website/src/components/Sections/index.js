d4ff955d39092b170290e3dfa5754f63456e1755
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2025-09-19 01:29:56 +0200

    Updated the 'S3' component #61

--- frontend/packages/volto-sevilla-imd-website/src/components/Sections/index.js
+++ frontend/packages/volto-sevilla-imd-website/src/components/Sections/index.js
@@ -3,7 +3,10 @@
  * @module components/Sections
  */
 
+import S1 from './s1';
+import S2 from './s2';
 import S3 from './s3';
 import S4 from './s4';
+import S5 from './s5';
 
-export { S3, S4 };
+export { S1, S2, S3, S4, S5 };
