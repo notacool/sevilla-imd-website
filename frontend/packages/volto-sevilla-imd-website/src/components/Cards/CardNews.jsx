fd3ba4ee0833f9189f70ed5ee68528c554bf6ed2
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2025-09-18 18:13:46 +0200

    Added more improvements about the 's4' component #62

---
+++ frontend/packages/volto-sevilla-imd-website/src/components/Cards/CardNews.jsx
@@ -0,0 +1,69 @@
+/**
+ * Card News component.
+ * @module components/Cards/CardNews
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import moment from 'moment';
+import DefaultImageSVG from '@plone/volto/components/manage/Blocks/Listing/default-image.svg';
+
+const CardNews = ({
+  title,
+  description,
+  image,
+  date,
+  isFeatured = false,
+  className,
+}) => {
+  return (
+    <div className={classNames('card-news', { featured: isFeatured }, className)}>
+      <div className="card-content">
+        {/* Background Layer */}
+        <div className="card-background">
+          <div className="bottom-bg" />
+          <div className="top-bg" />
+        </div>
+
+        {/* Card Image */}
+        <div className="card-image">
+          <div
+            className="image-wrapper"
+            style={{
+              backgroundImage: `url(${image || DefaultImageSVG})`,
+            }}
+          />
+        </div>
+
+        {/* Card Text Content */}
+        <div className="card-text">
+          <div className="indicator-line" />
+
+          <h3 className="card-title">
+            {title || 'Título de la noticia...lorem ipsum neque qui sit amet.'}
+          </h3>
+
+          <p className="card-description">
+            {description || 'Lorem ipsum dolor sit amet consectetur adipiscing elit condim entum justo quis pellen et dignissim amet...'}
+          </p>
+
+          <p className="card-date">
+            {date ? moment(date).format('DD/MM/YYYY') : '00/00/0000'}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+CardNews.propTypes = {
+  title: PropTypes.string,
+  description: PropTypes.string,
+  image: PropTypes.string,
+  date: PropTypes.string,
+  isFeatured: PropTypes.bool,
+  className: PropTypes.string,
+};
+
+export default CardNews;
